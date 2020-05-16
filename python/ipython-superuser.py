f04d3bb59a04f75e8bd1f25878a591911704143d
Kristoffer Larsen <kri@telia.net>
2020-05-16 19:42:05 +0200

    cd into the current path of the cli

--- python/ipython-superuser.py
+++ python/ipython-superuser.py
@@ -1,5 +1,6 @@
 import os
 import time
+import sys
 time.sleep(1)
 
 import ncs
@@ -8,6 +9,12 @@ ncs_sess_id=int(os.environ['NCS_MAAPI_USID'])
 maapi=ncs.maapi.Maapi()
 trans=maapi.attach(ncs_trans_id)
 root=ncs.maagic.get_root(trans)
+ipath=sys.argv[-1]
+if ipath:
+    trans.cd(ipath)
+    node=ncs.maagic.get_node(trans,ipath)
+else:
+    node=root
 print("+-----------------------------------------------------------------------------+")
 print("| You may reference the current transaction maagic YANG root object as 'root' |")
 print("| E.g.   In [1]: for dev in root.devices.device:                              |")
